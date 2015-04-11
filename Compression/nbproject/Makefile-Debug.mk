1194dad3b2af7e97a933e2837a37ba1135026c24
Roberts Slisans <rsxdalv@gmail.com>
2015-04-11 18:31:52 -0400

    Combined Compression algorithms, renamed Compress to Compression

--- Compression/nbproject/Makefile-Debug.mk
+++ Compression/nbproject/Makefile-Debug.mk
@@ -23,7 +23,7 @@ AS=as
 # Macros
 CND_PLATFORM=Visual_Studio_10-Windows
 CND_DLIB_EXT=dll
-CND_CONF=Release
+CND_CONF=Debug
 CND_DISTDIR=dist
 CND_BUILDDIR=build
 
@@ -35,11 +35,14 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/FileFormat.o \
 	${OBJECTDIR}/bitchain/main.o \
 	${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/CREATE.o \
 	${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/FILL.o \
 	${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/READ.o \
-	${OBJECTDIR}/main.o
+	${OBJECTDIR}/main.o \
+	${OBJECTDIR}/main_1.o \
+	${OBJECTDIR}/main_2.o
 
 
 # C Compiler Flags
@@ -66,33 +69,48 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/compress_1.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/compress_1 ${OBJECTFILES} ${LDLIBSOPTIONS}
 
+${OBJECTDIR}/FileFormat.o: FileFormat.c 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/FileFormat.o FileFormat.c
+
 ${OBJECTDIR}/bitchain/main.o: bitchain/main.cpp 
 	${MKDIR} -p ${OBJECTDIR}/bitchain
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bitchain/main.o bitchain/main.cpp
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/bitchain/main.o bitchain/main.cpp
 
 .NO_PARALLEL:${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/CREATE.o
 ${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/CREATE.o: faili/Patvaliga\ \(binarie\ faili\)/CREATE.cpp 
 	${MKDIR} -p ${OBJECTDIR}/faili  faili\)
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/CREATE.o faili/Patvaliga\ \(binarie\ faili\)/CREATE.cpp
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/CREATE.o faili/Patvaliga\ \(binarie\ faili\)/CREATE.cpp
 
 .NO_PARALLEL:${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/FILL.o
 ${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/FILL.o: faili/Patvaliga\ \(binarie\ faili\)/FILL.cpp 
 	${MKDIR} -p ${OBJECTDIR}/faili  faili\)
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/FILL.o faili/Patvaliga\ \(binarie\ faili\)/FILL.cpp
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/FILL.o faili/Patvaliga\ \(binarie\ faili\)/FILL.cpp
 
 .NO_PARALLEL:${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/READ.o
 ${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/READ.o: faili/Patvaliga\ \(binarie\ faili\)/READ.cpp 
 	${MKDIR} -p ${OBJECTDIR}/faili  faili\)
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/READ.o faili/Patvaliga\ \(binarie\ faili\)/READ.cpp
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/faili/Patvaliga\ \(binarie\ faili\)/READ.o faili/Patvaliga\ \(binarie\ faili\)/READ.cpp
 
 ${OBJECTDIR}/main.o: main.cpp 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
-	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cpp
+
+${OBJECTDIR}/main_1.o: main_1.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main_1.o main_1.cpp
+
+${OBJECTDIR}/main_2.o: main_2.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main_2.o main_2.cpp
 
 # Subprojects
 .build-subprojects:
