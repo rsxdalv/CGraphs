a9b5f27644580f74cfe76d460e9b3c6635fa7122
Roberts Slisans <roberts.slisans@gmail.com>
2016-12-02 19:42:58 +0000

    Formatted

--- src/Krask.cpp
+++ src/Krask.cpp
@@ -3,8 +3,9 @@
 // Pieliku komentārus lai kļūtu nedaudz skaidrāka darbība
 using namespace std;
 
-struct link {
-  int source, size;
+struct link
+{
+    int source, size;
 };
 
 #define MAX_VALUE 2048
@@ -13,7 +14,6 @@ struct link {
 
 int main()
 {
-
     int Matrix[SIZED][SIZED]={
         0,	6,	_,	11,	5,	_,	_,
         6,	0,	15,	18,	8,	_,	_,
@@ -25,64 +25,65 @@ int main()
 
     int mark[SIZED] = {0};
     link Mi[SIZED] = {MAX_VALUE};
-    for(int a=0;a<SIZED;a++)
-        Mi[a] = (struct link) {.source = -1, .size = MAX_VALUE};
+    for (int a = 0; a < SIZED; a++)
+        Mi[a] = (struct link){.source = -1, .size = MAX_VALUE};
 
-    int next=0;
-    int length=0;
+    int next = 0;
+    int length = 0;
 
-    int maing[SIZED]={0};
-    int groupn=0;
+    int maing[SIZED] = {0};
+    int groupn = 0;
 
-    for(int count = 1; count < SIZED; count++)
+    for (int count = 1; count < SIZED; count++)
     {
 
-        int minn=_;
-        int alpha=-1,beta=-1;
-        for(int i=0;i<SIZED;i++)
+        int minn = _;
+        int alpha = -1, beta = -1;
+        for (int i = 0; i < SIZED; i++)
         { // Pret katru virsotni
-            for(int k=i+1;k<SIZED;k++)
+            for (int k = i + 1; k < SIZED; k++)
             {
-                if(mark[k]==0 || mark[i]==0 || mark[i]!=mark[k])
-                //Ja viena no virstonem ir neiezimeta
-                //if((!mark[k])||mark[i]!=mark[k])
-                //atskirigas grupas virsotnes
-                if(Matrix[i][k]<minn)
-                {
-                //ir loks mazaks par min
-                    alpha=i;beta=k;
-                    minn=Matrix[i][k];
-                }
+                if (!mark[k] || mark[i] != mark[k])
+                    //Ja viena no virstonem ir neiezimeta
+                    //atskirigas grupas virsotnes
+                    if (Matrix[i][k] < minn)
+                    {
+                        //ir loks mazaks par min
+                        alpha = i;
+                        beta = k;
+                        minn = Matrix[i][k];
+                    }
             }
-            //if(mark[i])
         }
-        if(alpha==-1||beta==-1) cout<<"Nesasniedzama virsotne!"<<endl;
-        if(minn==_) cout<<"kluuda!"<<endl;
+        if (alpha == -1 || beta == -1)
+            cout << "Nesasniedzama virsotne!" << endl;
+        if (minn == _)
+            cout << "kluuda!" << endl;
         else
-            length+=Matrix[alpha][beta];
-        if(mark[alpha]==0)
+            length += Matrix[alpha][beta];
+        if (mark[alpha] == 0)
         {
-            if(mark[beta]==0)
+            if (mark[beta] == 0)
             {
-                mark[alpha]=mark[beta]=++groupn;
+                mark[alpha] = mark[beta] = ++groupn;
             }
             else
-                mark[alpha]=mark[beta];
+                mark[alpha] = mark[beta];
         }
-        else if(mark[beta]==0)
-            mark[beta]=mark[alpha];
+        else if (mark[beta] == 0)
+            mark[beta] = mark[alpha];
         else
         {
-            int repl=mark[beta];
-            int rep=mark[alpha];
-            for(int x=0;x<SIZED;x++)
-                if(mark[x]==repl)
-                    mark[x]=rep;
+            int repl = mark[beta];
+            int rep = mark[alpha];
+            for (int x = 0; x < SIZED; x++)
+                if (mark[x] == repl)
+                    mark[x] = rep;
         }
         printf("Link %c <-> %c\n", alpha + 'A', beta + 'A');
     }
 
-    cout<<"Length : "<<length<<endl;
+    cout << "Length : " << length << endl;
 
     return 0;
 }
